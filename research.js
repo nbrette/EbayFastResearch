eb0bb6230367e746068ed925fbdb1f97f0ff1b6e
Nicolas Brette <n.brette@gmail.com>
2021-10-29 16:33:56 +0200

    features and design

--- research.js
+++ research.js
@@ -1,32 +1,81 @@
+const URL_BEGGINING = "https://www.ebay";
+const URL_SECOND_PART = "/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw="
+const URL_LAST_PART="&_sacat=0";
+const COUNTRY_LOCATION="Country";
+const EU_LOCATION="EU";
+const WORLD_LOCATION="World";
+
 document.getElementById('btn').onclick = function(){
-    var keywords = document.getElementById('research').value;
-    var extension = document.getElementById('extension').value;
-    var sold = document.querySelector('#sold').checked;
+    var form = document.forms["ebayResearch"];
+
+    var keywords = form.elements["research"].value;
+    var extension = form.elements["extension"].value;
+    var location = form.elements["location"].value;
+    var sold = form.elements["checkSold"].checked;
 
     var tabKeywords = keywords.split(" ");
-    url = createUrl(extension, tabKeywords, sold);
+    url = createUrl(extension, tabKeywords, sold, location);
     chrome.tabs.create({ url: url });
 }
 
-function createUrl(extension, researchKeywords, sold){
-    urlBeggining = "https://www.ebay.";
-    urlSecondPart = "/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw="
-    urlLastPart = "&_sacat=0";
-    url = urlBeggining+extension+urlSecondPart;
+document.getElementById('extension').onchange = function() {
+    if (document.getElementById('extension').value == ".COM"){
+        document.getElementById('location').selectedIndex = 0;
+        document.getElementById('location').disabled = true;
+    }
+    else{
+        document.getElementById('location').disabled = false;
+    }
+}
+
+function createUrl(extension, researchKeywords, sold, location){
+    url = URL_BEGGINING+extension+URL_SECOND_PART;
+    url += keywordsToString(researchKeywords);
+    url+=URL_LAST_PART;
+    url += getSold(sold);
+    url += getLocationRestriction(location);
+
+
+    return url;
+}
+
+function keywordsToString(researchKeywords){
+    var keywordsString ="";
+    
     if (researchKeywords.length > 1){
         for (let index = 0; index < researchKeywords.length; index++) {
-            url += researchKeywords[index];
+            keywordsString += researchKeywords[index];
             if (index != researchKeywords.length-1){
-                url += "+"
+                keywordsString += "+"
             }
         }
     }else if (researchKeywords.length == 1){
-        url += researchKeywords[0];
+        keywordsString += researchKeywords[0];
     }
 
-    if (sold){
-        url += "&rt=nc&LH_Sold=1&LH_Complete=1";
+    return keywordsString;
+}
+
+function getLocationRestriction(location){
+    var countryRestrictionString = "";
+    if (location == COUNTRY_LOCATION){
+        countryRestrictionString+= "&rt=nc&LH_PrefLoc=1";
     }
-    return url;
-    //https://www.ebay.fr/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=gyarados+130&_sacat=0
+    else if (location == EU_LOCATION){
+        countryRestrictionString+= "&rt=nc&LH_PrefLoc=3";
+    }
+    else if (location == WORLD_LOCATION){
+        countryRestrictionString+= "&rt=nc&LH_PrefLoc=2";
+    }
+
+    return countryRestrictionString;
+}
+
+function getSold(sold){
+    var soldString =""
+    if (sold){
+        soldString += "&rt=nc&LH_Sold=1&LH_Complete=1";
+    }
+
+    return soldString;
 }
